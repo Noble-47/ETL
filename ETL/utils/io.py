f992189a09ae8f074d8b5d0c5117096c5c05df0a
Noble47 <noblebest56@gmail.com>
2024-05-27 19:36:10 +0100

    seperated utils to different componenets

---
+++ ETL/utils/io.py
@@ -0,0 +1,60 @@
+from pandas import read_csv, read_excel
+from pandas import DataFrame
+from pathlib import Path
+import os
+
+class IOMixin:
+
+    extension_reader = {".xlsx": read_excel, ".csv": read_csv}
+    extension_and_writer = {"excel" : ("xlsx", DataFrame.to_excel), "csv" : ("csv", DataFrame.to_csv)}
+
+    def setup_data_dir(self, data_dir):
+        data_dir = data_dir or self.default_data_dir
+        self.data_dir = data_dir if isinstance(data_dir, Path) else Path(data_dir)
+        # check if program have write permission
+     #   if not os.access(data_dir, mode=os.W_OK):
+     #       self.logger.critical(
+     #           f"Permission Error: Do not have permission to write to {self.data_dir}"
+     #       )
+     #       # Raise permission error
+     #       raise Exception
+
+
+    def setup_save_dir(self, save_dir):
+        save_dir = save_dir or self.default_save_dir
+        self.save_dir = save_dir if isinstance(save_dir, Path) else Path(save_dir)
+        if not self.save_dir.is_dir():
+            self.save_dir.mkdir(parents=True, exist_ok=True)
+
+    def setup_directories(self, data_dir, save_dir):
+        self.setup_data_dir(data_dir)
+        self.setup_save_dir(save_dir)
+
+    def get_extension_and_writer(self):
+        return self.extension_and_writer.get(self.save_file_type)
+
+    def write(self, name, data):
+        ext, writer = self.get_extension_and_writer()
+        filename = self.save_dir / f"{name}.{ext}"
+        writer(data, filename, index=False)
+
+    def fetch(self):
+        try:
+            files = [f for f in self.data_dir.iterdir() if f.is_file()]
+        except Exception as e:
+            self.logger.exception(
+                    "Encountered an error trying to read %s", self.data_dir
+                )
+            raise e
+        else:
+            self.logger.info("%d files found in %s filder", len(files), self.data_dir.name)
+
+        datasets = [self.read(f) for f in files]
+        return datasets
+
+    def read(self, fn):
+        ext = fn.suffix
+        reader = self.extension_reader.get(ext)
+        return reader(fn)
+
+                                               
---
+++ ETL/utils/log.py
@@ -0,0 +1,41 @@
+import datetime as dt
+import logging
+import json
+
+class JsonFormatter(logging.Formatter):
+
+    def __init__(self, *, fmt_keys = None):
+        super().__init__()
+        self.fmt_keys = fmt_keys if fmt_keys is not None else dict()
+
+    def format(self, record):
+        message = self._prepare_log_dict(record)
+        return json.dumps(message, default=str)
+
+    def _prepare_log_dict(self, record):
+        fields = {
+                "message" : record.getMessage(),
+                "timestamp" : dt.datetime.fromtimestamp(
+                        record.created,
+                        tz = dt.timezone.utc
+                    ).isoformat()
+            }
+
+        if record.exc_info is not None:
+            fields['exc_info'] = self.formatException(record.exc_info)
+
+        if record.stack_info is not None:
+            fields['stack_info'] = self.formatStack(record.stack_info)
+
+        message = dict()
+
+        for key, val in self.fmt_keys.items():
+            msg_val = fields.pop(val, None)
+            if msg_val is None:
+                msg_val = getattr(record, val)
+            message[key] = msg_val
+
+        message.update(fields)
+
+        return message
+
